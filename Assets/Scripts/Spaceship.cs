b6697a1117165eac03c6982d221976abc19b1460
Indrasish Roy <118255208+IndrasishRoy@users.noreply.github.com>
2025-05-20 11:33:41 +0530

    Update Spaceship.cs

--- Assets/Scripts/Spaceship.cs
+++ Assets/Scripts/Spaceship.cs
@@ -22,17 +22,30 @@ public class Spaceship : MonoBehaviour
         float move = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         float rotation = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
 
-        if (Input.GetKey(KeyCode.LeftShift) && transform.position.y < 20f)
+        /*if (Input.GetKey(KeyCode.LeftShift) && transform.position.y < 20f)
         {
             transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
         }
         else if (Input.GetKey(KeyCode.LeftControl) && transform.position.y > 2.6f)
         {
             transform.Translate(Vector3.down * moveSpeed * Time.deltaTime);
+        }*/
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.LeftControl))
+        {
+            transform.Translate(Vector3.down * moveSpeed * Time.deltaTime);
         }
 
         transform.Translate(Vector3.forward * move);
         transform.Rotate(Vector3.up * rotation);
+
+        Vector3 newPos = transform.position;
+        newPos.y = Mathf.Clamp(transform.position.y, 2.6f, 20f);
+        transform.position = newPos;
     }
 
     private void ShootLazer()
